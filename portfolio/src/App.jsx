b720a7f4a8071666972245bd86d53ee3a3e3acd3
izzy-droid <isdm908@gmail.com>
2025-10-23 21:29:17 -0400

    setting up files

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+//idea: an 2000s old computer 3d model to click around as each folder/file is a project/other thing fml I work too hard lol
+function App() {
+
+
+  return (
+    <>
+      <div className=" top-[117px] lg:top-[344px] bg-sky-600 min-h-25">
+        <div className="flex flex-col md:flex-row">
+        <p>Home</p>
+        <p>About</p>
+        <p>Projects</p>
+        
+        </div>
+       
+      </div>
+      <div className=" top-[117px] lg:top-[344px]  ">
+        <p>Hey there! My name is Isabella</p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
